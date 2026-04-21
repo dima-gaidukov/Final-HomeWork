d9abd26d0687a614e44adbc682086609bb520686
dmitrijgajdukov <dima-gaidukov@mail.ru>
2026-04-21 15:57:18 +0300

    test

--- event-manager/src/main/java/dev/sorokin/eventmanager/Main.java
+++ event-manager/src/main/java/dev/sorokin/eventmanager/Main.java
@@ -1,4 +1,10 @@
 package dev.sorokin.eventmanager;
 
+import org.springframework.boot.SpringApplication;
+
 public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
